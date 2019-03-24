cb24c1c5d80753d9cf9900af8b2aa8642ad696da
jungledj <junglaq@hotmail.com>
2019-03-24 17:45:41 -0500

    Implementacion vista login

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -1,2 +1,32 @@
 <?php
 
+class ControladorUsuarios {
+
+/*===========================
+INGRESO DE USUARIO
+============================*/
+	public function ctrIngresoSeguro(){
+
+		if(isset($_POST["ingUsuario"])){
+
+				if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
+				   preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])){
+
+				   	$tabla = "usuarios";
+
+				    $item = "usuario";
+
+				    $valor = $_POST["ingresoUsuario"];
+
+				    $respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
+
+			}
+
+
+		}
+
+
+	}
+
+
+}
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -56,60 +56,67 @@
 CUERPO DOCUMENTO            =
 ============================================= -->
 
-<body class="hold-transition skin-blue sidebar-collapse sidebar-mini">
+<body class="hold-transition skin-blue sidebar-collapse sidebar-mini login-page">
 
-  <!-- Site wrapper -->
-  <div class="wrapper">
 
   <?php
 
-    /*=============================================
-    CABEZOTE
-    =============================================*/   
-    include "modulos/cabezote.php";
- 
-    /*=============================================
-    MENU
-    =============================================*/
-    include "modulos/menu.php";
+    if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok") {
 
-    /*=============================================
-    CONTENIDO - INICIO
-    =============================================*/
-    if(isset($_GET["ruta"])){
+      echo '<div class="wrapper">';
 
-      if($_GET["ruta"] == "inicio" ||
-         $_GET["ruta"] == "usuarios" ||
-         $_GET["ruta"] == "categorias" ||
-         $_GET["ruta"] == "productos" ||
-         $_GET["ruta"] == "clientes" ||
-         $_GET["ruta"] == "ventas" ||
-         $_GET["ruta"] == "crear-venta" ||
-         $_GET["ruta"] == "reportes"){
+      /*=============================================
+      CABEZOTE
+      =============================================*/   
+      include "modulos/cabezote.php";
+   
+      /*=============================================
+      MENU
+      =============================================*/
+      include "modulos/menu.php";
 
-         include "modulos/".$_GET["ruta"].".php";
- 
+      /*=============================================
+      CONTENIDO - INICIO
+      =============================================*/
+      if(isset($_GET["ruta"])){
+
+        if($_GET["ruta"] == "inicio" ||
+           $_GET["ruta"] == "usuarios" ||
+           $_GET["ruta"] == "categorias" ||
+           $_GET["ruta"] == "productos" ||
+           $_GET["ruta"] == "clientes" ||
+           $_GET["ruta"] == "ventas" ||
+           $_GET["ruta"] == "crear-venta" ||
+           $_GET["ruta"] == "reportes"){
+
+           include "modulos/".$_GET["ruta"].".php";
+   
+        } else {
+
+          include "modulos/404.php";
+
+        } 
+
       } else {
 
-        include "modulos/404.php";
+          include "modulos/inicio.php";
 
-      } 
+      }
+
+       /*=============================================
+      FOOTER
+      =============================================*/
+      include "modulos/footer.php";
+
+      echo '</div>';
 
     } else {
 
-        include "modulos/inicio.php";
+      include "modulos/login.php";
 
     }
 
-     /*=============================================
-    FOOTER
-    =============================================*/
-    include "modulos/footer.php";
-
   ?>
-
-  </div>
-  <!-- ./wrapper -->
 
   <script src="vistas/js/plantilla.js"></script>
 </body>
