d7722a5321657f5e4533a4c5d1821c39ee51174a
jungledj <junglaq@hotmail.com>
2019-03-24 20:49:29 -0500

    funcionalidad basica de loguin

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -1,11 +1,11 @@
 <?php
 
-class ControladorUsuarios {
+class ControladorUsuarios{
 
 /*===========================
 INGRESO DE USUARIO
 ============================*/
-	public function ctrIngresoSeguro(){
+	static public function ctrIngresoUsuario(){
 
 		if(isset($_POST["ingUsuario"])){
 
@@ -15,10 +15,28 @@ INGRESO DE USUARIO
 				   	$tabla = "usuarios";
 
 				    $item = "usuario";
-
-				    $valor = $_POST["ingresoUsuario"];
+				    $valor = $_POST["ingUsuario"];
 
 				    $respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
+
+				    if($respuesta["usuario"] == $_POST["ingUsuario"] && $respuesta["password"] == $_POST["ingPassword"]){
+
+				    	$_SESSION["iniciarSesion"] = "ok";
+
+				    	echo '<script>
+
+							window.location = "inicio";
+
+				    	</script>';
+
+
+				    } else {
+
+
+				    	echo '<br><div class="alert alert-danger">Error al ingresar, vuelve a intentarlo</div>';
+
+				    }
+				    
 
 			}
 
--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,13 +1,16 @@
 <?php
 
-/**
- * 
- */
 class Conexion{
-	
-	public function conectar(){
 
-		$link = new POD("","",""); 
+	static public function conectar(){
+
+		$link = new PDO("mysql:host=localhost;dbname=pos-copia",
+			            "root",
+			            "root");
+
+		$link->exec("set names utf8");
+
+		return $link;
 
 	}
 
